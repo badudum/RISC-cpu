4a075e273ada4400dcf3be02f53cd2122af0ece9
maplesyrup-0606 <80257771+maplesyrup-0606@users.noreply.github.com>
2024-01-10 21:03:56 -0800

    Added Signal assigns for N (negative) and V(overflow / underflow) detection

--- alu.sv
+++ alu.sv
@@ -1,6 +1,6 @@
-module ALU(input logic [15:0] val_A, input logic [15:0] val_B, input logic [1:0] ALU_op, output logic [15:0] ALU_out, output logic Z);
-
-
+module ALU(input logic [15:0] val_A, input logic [15:0] val_B, input logic [1:0] ALU_op, output logic [15:0] ALU_out, output logic Z, output logic N, output logic V);
+  
+  assign N = ALU_out[15];
   always @(*) begin
     case (ALU_op)
       2'b00: ALU_out = val_A + val_B;
@@ -12,6 +12,13 @@ module ALU(input logic [15:0] val_A, input logic [15:0] val_B, input logic [1:0]
       Z = 1;
     end else begin
       Z = 0;
+    end
+    
+    if((ALU_op == 2'b00 && ((!val_A[15] && !val_B[15] && ALU_out[15]) || (val_A[15] && val_B[15] && !ALU_out[15]))) 
+       || (ALU_op == 2'b01 && ((val_A[15] && !val_B[15] && !ALU_out[15]) || (!val_A[15] && val_B[15] && ALU_out[15])))) begin
+      V = 1;
+    end else begin
+      V = 0;
     end
   end
   
