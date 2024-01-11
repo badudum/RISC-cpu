420d5a843fce5a5858a705687ab18d910c300052
maplesyrup-0606 <80257771+maplesyrup-0606@users.noreply.github.com>
2024-01-10 20:23:57 -0800

    Update shifter.sv
    Error in logical shift right (op == 2'b10)

--- shifter.sv
+++ shifter.sv
@@ -3,7 +3,7 @@ module shifter(input logic [15:0] shift_in, input logic [1:0] shift_op, output l
     case (shift_op)
       2'b00: shift_out = shift_in;
       2'b01: shift_out = shift_in << 1;
-      2'b10: shift_out = shift_in << 1;
+      2'b10: shift_out = shift_in >> 1;
       2'b11: shift_out = shift_in >>> 1;
     endcase
   end
